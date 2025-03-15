a522c41f7e99ae9026d28e78628d80ffb66fe234
swethagopisetty <swethagopisetty8@gmail.com>
2025-03-15 10:55:08 -0500

    Add files via upload

---
+++ Sample_Outputs/Sample_Generation_Query.sql
@@ -0,0 +1,5 @@
+--Sample Output Generation
+select generate_analysis_csv('Orders','C:\Users\Public\Downloads\Orders.csv');
+select generate_analysis_csv('Products','C:\Users\Public\Downloads\Products.csv');
+select generate_analysis_csv('Departments','C:\Users\Public\Downloads\Departments.csv');
+select generate_analysis_csv('Aisles','C:\Users\Public\Downloads\Aisles.csv');
