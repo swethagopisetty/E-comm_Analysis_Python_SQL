6cae36fa78fac85d80bd00a8239098254abef133
swethagopisetty <swethagopisetty8@gmail.com>
2025-03-15 11:53:38 -0500

    Add files via upload

--- Sample_Outputs/Sample_Generation_Query.sql
+++ Sample_Outputs/Sample_Generation_Query.sql
@@ -2,4 +2,4 @@
 select generate_analysis_csv('Orders','C:\Users\Public\Downloads\Orders.csv');
 select generate_analysis_csv('Products','C:\Users\Public\Downloads\Products.csv');
 select generate_analysis_csv('Departments','C:\Users\Public\Downloads\Departments.csv');
-select generate_analysis_csv('Aisles','C:\Users\Public\Downloads\Aisles.csv');
+select generate_analysis_csv('Aisles','C:\Users\Public\Downloads\Top_Aisles.csv');
